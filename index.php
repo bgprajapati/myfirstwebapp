9ac0ef33b94f9c96024ebea9849ac35becc0bbf7
bgprajapati <102646159+bgprajapati@users.noreply.github.com>
2022-03-30 13:03:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,28 +16,7 @@ echo "<h1> This is my first app</h1>";
     <button type="submit" value="Submit">Submit</button>
    
   
-  <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  <input type="submit" Value="Connect to MySQL Server & Insert data in a table">
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-$servername = "bgpsqlserver.mysql.database.azure.com";
-$username = "bgp";
-$password = "abcd@123";
-$dbname = "bgpdb";
-  
-  
-    // Create connection
-$conn = newmysqli($servername, $username, $password,$dbname);
-// Check connection
-if ($conn->connect_error) {​
-die("Connection failed: ".$conn->connect_error);
-}
-  
- 
-echo"Connected successfully<br>";
-    ?>
-  
-</form>
+
 
 
 </body>

1f6a13aa0c27e152ea55d06faa4d5190edf8e7f1
bgprajapati <102646159+bgprajapati@users.noreply.github.com>
2022-03-30 12:35:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ My First App
 echo "<h1> This is my first app</h1>";
   
 ?>
- <table> 
+ <table border=1> 
    <tr><td>User Name:</td><td><input type=text name=uname></td></tr>
    
    <tr><td>Password:</td><td><input type=password name=pwd></td></tr>

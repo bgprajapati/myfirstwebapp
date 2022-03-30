040fb0c4aa181a8ba4097f739515312fa302053c
bgprajapati <102646159+bgprajapati@users.noreply.github.com>
2022-03-30 12:34:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ echo "<h1> This is my first app</h1>";
  <table> 
    <tr><td>User Name:</td><td><input type=text name=uname></td></tr>
    
-   <tr><td>Password :</td><td><input type=password name=pwd></td></tr>
+   <tr><td>Password:</td><td><input type=password name=pwd></td></tr>
    <tr><td> <button type="submit" value="Submit">Submit</button></td></tr>
    </table>
 </body>

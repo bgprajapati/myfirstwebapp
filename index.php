55c3e52f1ddff2287bf3090ab7d5db3d8540009c
bgprajapati <102646159+bgprajapati@users.noreply.github.com>
2022-03-30 13:01:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,8 @@ echo "<h1> This is my first app</h1>";
    <tr><td>User Name:</td><td><input type=text name=uname></td></tr>
    
    <tr><td>Password:</td><td><input type=password name=pwd></td></tr>
-   </table>
-   <tr><td align="center"> <button type="submit" value="Submit">Submit</button></td></tr>
+   </table><br>
+    <button type="submit" value="Submit">Submit</button>
    
   
   <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
@@ -31,7 +31,9 @@ $conn = newmysqli($servername, $username, $password,$dbname);
 // Check connection
 if ($conn->connect_error) {​
 die("Connection failed: ".$conn->connect_error);
-}​
+}
+    ?>
+  ​
 /*echo"Connected successfully<br>";
 $sql = "INSERTINTO student VALUES (4, 'Savani', 'Shivraj', '86565');";
 $sql.="INSERTINTO Student VALUES (7, 'twinkal', 'karad', '55');";
@@ -39,7 +41,7 @@ $sql.="INSERTINTO Student VALUES (8, 'pooja', 'karad', '55');";
 $sql.="INSERTINTO Student VALUES (9, 'komal', 'abad', '55');";
 $sql.="INSERTINTO Student VALUES (10, 'shreya', 'abad', '55');";
 $sql.="INSERTINTO Student VALUES (11, 'mayuri', 'avasari', '55')";*/
-  ?>
+  
 </form>
 
 
